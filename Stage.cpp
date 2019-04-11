4ef75b8995b120d44c2071c96f092c91cff88e21
Nosuriba@11 <kitagawa_taiko@icloud.com>
2019-04-12 02:00:42 +0900

    ステージﾃﾞｰﾀの登録を参考にして書いてみた。

--- Stage.cpp
+++ Stage.cpp
@@ -1,3 +1,4 @@
+#include <DxLib.h>
 #include "stdio.h"
 #include "Stage.h"
 
@@ -5,6 +6,36 @@
 
 Stage::Stage()
 {
+	/// 読み込むステージファイルの指定
+	int h = DxLib::FileRead_open("stage/stage1.fmf", false);
+
+	DxLib::FileRead_read(&fmfData, sizeof(fmfData), h);
+
+	/// 縦横のﾏｯﾌﾟﾃﾞｰﾀを読み込んでいる
+	std::vector<char> tmp;
+	tmp.resize(fmfData.mapWidth * fmfData.mapHeight);
+	DxLib::FileRead_read(&tmp[0], tmp.size(), h);
+
+	DxLib::FileRead_close(h);
+
+	/// ステージの大きさと中心を設定している
+	stageRange.size.width = fmfData.mapWidth * fmfData.chipW * 2;
+	stageRange.size.height = fmfData.mapHeight * fmfData.chipH * 2;
+	stageRange.center.x = stageRange.Width() / 2;
+	stageRange.center.y = stageRange.Height() / 2;
+
+	/// ﾏｯﾌﾟのｻｲｽﾞ分、敵のﾃﾞｰﾀｻｲｽﾞを確保している
+	enemyData.resize(fmfData.mapWidth * fmfData.mapHeight);
+
+	for (int y = 0; y < fmfData.mapHeight; y++)
+	{
+		for (int x = 0; x < fmfData.mapWidth; x++)
+		{
+			enemyData[x * fmfData.mapHeight + y] = tmp[y * fmfData.mapWidth + x];
+		}
+	}
+
+	readX = 0;
 }
 
 
@@ -12,15 +43,28 @@ Stage::~Stage()
 {
 }
 
-void Stage::Load()
+const Rect & Stage::GetStageRange() const
 {
-	/*FILE *fp;
-	char fname[] = "";
-	fp fopen()
-*/
-
+	return stageRange;
 }
 
-void Stage::Save()
+const std::vector<char> Stage::GetEnemyData(int minX, int maxX)
 {
+	/// 敵ﾃﾞｰﾀを最初と最後を登録している
+	int L = max((minX / fmfData.chipW * 2), readX);
+	int R = maxX / (fmfData.chipW * 2) + 1;
+
+	/// 敵の情報がない場合
+	if (R <= readX)
+	{
+		return std::vector<char>();
+	}
+
+	/// 敵の情報を登録している
+	auto idxL = L * fmfData.mapHeight;
+	auto idxR = min(R * fmfData.mapHeight, enemyData.size());
+	auto begin = enemyData.begin() + idxL;
+	auto end = min(enemyData.begin() + idxR, enemyData.end());
+	readX = R;
+	return std::vector<char>(begin, end);
 }
--- Stage.h
+++ Stage.h
@@ -1,11 +1,38 @@
 #pragma once
+#include <vector>
+#include "Geometry.h"
+
+struct FMF
+{
+	int identifier;
+	int size;
+	int mapWidth;
+	int mapHeight;
+	char chipH;
+	char chipW;
+	char layerCount;
+	char bitCount;
+};
+
 class Stage
 {
 public:
 	Stage();
 	~Stage();
-	void Load();
-	void Save();
+
+	//ステージの範囲を渡す
+	const Rect& GetStageRange()const;
+	//現在の画面内の敵情報を渡す
+	const std::vector<char> GetEnemyData(int minX, int maxX);
+private:
+	Rect stageRange;
+	int readX;
+
+	//ステージ情報
+	FMF fmfData;
+	//ステージのエネミー情報
+	std::vector<char> enemyData;
 
 };
+
 
